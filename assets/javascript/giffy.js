d5d9c9b44af3d6ab4d30e41475783c2e4791292a
Tabbs18 <tabithamclaughlin500@yahoo.com>
2021-12-07 19:30:11 -0500

    updated API which fixed problem

--- assets/javascript/giffy.js
+++ assets/javascript/giffy.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
       $(this).addClass("active");
   
       var type = $(this).attr("data-type");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=Yjmtlr74t0eM9ru2pOIr02HchgPTe5A5&limit=10";
   
       $.ajax({
         url: queryURL,
